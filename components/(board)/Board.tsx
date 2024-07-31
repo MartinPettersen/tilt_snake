5a154ea8c387dd8a5e534eaecdf3d77a679256e6
MartinPettersen <searchingtheskyfor@gmail.com>
2024-07-31 13:36:40 +0200

    fix small bug

--- components/(board)/Board.tsx
+++ components/(board)/Board.tsx
@@ -88,7 +88,7 @@ const Board = () => {
             let newY = segment.y;
 
             console.log(newX);
-            if (newX < 0 || newX > width || newY < 0 || newY > height) {
+            if (newX < 0 || newX > (width) || newY < 0 || newY > height) {
               setGameRunning(false);
             }
 
@@ -167,9 +167,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    width: width,
-    height: height,
-    backgroundColor: "pink",
+    backgroundColor: "white",
     borderWidth: 1,
     borderColor: "red"
   },
